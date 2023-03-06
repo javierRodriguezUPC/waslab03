ffb6b0ad3dfe5474ff0e8ed27be33e9bb61d6f4a
javir <javir@javi>
2023-03-06 21:21:10 +0100

    Tasca #6 finalitzada amb un error

--- WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
+++ WASLab03_SimpleMastodonClient/src/fib/asw/waslab03/Tasca_6.java
@@ -4,6 +4,8 @@ import org.apache.hc.client5.http.fluent.Request;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import jdk.nashorn.internal.parser.JSONParser;
+
 public class Tasca_6 {
 
 	public static void main(String[] args) {
@@ -19,32 +21,38 @@ public class Tasca_6 {
 					.returnContent()
 					.asString();
 			
-
+			
 			JSONArray result = new JSONArray(trends);
 			
 			for (int i = 0; i < result.length(); i++) {
 			    JSONObject explrObject = result.getJSONObject(i);
-			    String name = (String) explrObject.get("name");
-				System.out.println(name);
+			    String name = (String) explrObject.getString("name");
+			    
+
+				System.out.println("*************************************************");
+				System.out.println("tags: " + name);
+				System.out.println("*************************************************");
 				
 				String tweets = Request.get("https://mastodont.cat/api/v1/timelines/tag/" + name + "?limit=5")
 						.addHeader("Authorization","Bearer "+TOKEN)
 						.execute()
 						.returnContent()
 						.asString();
-
 				
 				
-				/*JSONArray JSONTweets = new JSONArray(tweets);
-			    System.out.println(JSONTweets);		
-			    
-			    
+				JSONArray JSONTweets = new JSONArray(tweets);
 			    
 				for (int j = 0; j < JSONTweets.length(); j++) {
 				    JSONObject tweet = JSONTweets.getJSONObject(j);
-				    //String asasas = (String) explrObject2.get("display_name");
-				    //System.out.println(result2);					
-				}*/
+				    //String display_name = tweet.getString("display_name");
+				    JSONObject account = tweet.getJSONObject("account");
+				    String acct = (String) account.getString("acct");
+				    String content = (String) tweet.getString("content");
+					content = content.replaceAll("<[^>]*>", "");
+
+				    System.out.println("- " + "display_name" + "(" + acct + "): " + content);	
+				    System.out.println("-------------------------------------------------");			
+				}
 				
 			}
 			
